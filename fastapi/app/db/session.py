e85368b15ba815c5707440a87c36683f1f082bf7
repen7ant <ilyarepentant@icloud.com>
2026-04-11 15:30:23 +0500

    fix(fastapi): move base to session, add async

--- fastapi/app/db/session.py
+++ fastapi/app/db/session.py
@@ -1,12 +1,18 @@
 from core.config import settings
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 engine = create_async_engine(settings.DATABASE_URL)
-SessionLocal = async_sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
 )
 
 
 async def get_db():
-    async with SessionLocal() as session:
+    async with AsyncSessionLocal() as session:
         yield session
