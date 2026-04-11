fa2a87bd9a51e34a30743fb9974fbd656766f06c
repen7ant <ilyarepentant@icloud.com>
2026-04-11 16:29:52 +0500

    feat(laravel): add db and redis health check

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/health/redis', function () {
+    Redis::set('laravel_ping', 'pong', 'EX', 10);
+    $val = Redis::get('laravel_ping');
+    return response()->json(['status' => 'ok', 'value' => $val]);
+});
+
+Route::get('/health/db', function () {
+    $result = DB::select('SELECT 1 as result');
+    return response()->json(['status' => 'ok', 'result' => $result]);
 });
