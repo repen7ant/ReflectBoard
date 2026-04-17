8f231d8e1f1e8f7acea440d9e47a0b8d2937cb4d
repen7ant <ilyarepentant@icloud.com>
2026-04-17 22:45:14 +0500

    feat: add auth (default + github oauth)

---
+++ laravel/app/Http/Controllers/Auth/GitHubController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
+
+class GitHubController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function callback()
+    {
+        $githubUser = Socialite::driver('github')->user();
+
+        $user = User::updateOrCreate([
+            'email' => $githubUser->email,
+        ], [
+            'name' => $githubUser->name ?? $githubUser->nickname,
+            'github_id' => $githubUser->id,
+            // Если юзер создается впервые, даем ему случайный пароль для безопасности
+            'password' => bcrypt(Str::random(24))
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->intended('/board');
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,12 +19,16 @@ Route::get('/health/db', function () {
     return response()->json(['status' => 'ok', 'result' => $result]);
 });
 
-Route::get('/board', fn() => view('board'))->name('board');
+Route::get('/auth/github/redirect', [GitHubController::class, 'redirect'])->name('github.login');
+Route::get('/auth/github/callback', [GitHubController::class, 'callback']);
 
 Route::middleware('auth')->group(function () {
+    Route::get('/board', fn() => view('board'))->name('board');
     Route::get('/done', fn() => view('done'))->name('done');
     Route::get('/analytics', fn() => view('analytics'))->name('analytics');
 });
 
 Route::get('/login', fn() => 'login page coming soon')->name('login');
 Route::get('/register', fn() => 'register page coming soon')->name('register');
+
+require __DIR__.'/auth.php';
